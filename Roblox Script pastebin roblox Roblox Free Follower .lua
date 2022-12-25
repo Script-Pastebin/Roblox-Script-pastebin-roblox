8da54dcc577c7739756cbbb648f8ed827f1b5eb7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:37:52 +0100

    commit zero

---
+++ Roblox Script pastebin roblox Roblox Free Follower .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							First you need to get your roblox profile id. Id is in the roblox profile url section. Then go to the cheat site we gave, paste your profile id to the follower section. You can send followers in this way.
+						
+]]
